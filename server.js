cd78a6d02975f13392514a069312899cac899811
timcs <timcs1274@gmail.com>
2023-01-13 13:24:33 -0500

    add function to change time from military time to standard

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static('public'))
+
 // sendFile will go here
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/public'));
 });
 
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
+
