0fba13cd7f5d864279e03ac0091bcce2ef3c94f8
timcs <timcs1274@gmail.com>
2023-01-18 23:05:07 -0500

    function to append imgs to html page, also serve static imgs in server js

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
 app.use(express.static('public'))
 
 // sendFile will go here
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/public'));
 });
+
+app.get("/images", (req, res) => {
+  res.render("images", { images: imgArr });
+});
+
 
 
 app.listen(port);
